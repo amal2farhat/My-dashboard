6cf1430ea3074c72d5642f38d030cc3210ca8090
amal2farhat <yasminafarhat@hotmail.com>
2024-06-24 23:52:19 +0300

    Add files via upload

---
+++ includes/navbar.php
@@ -0,0 +1,17 @@
+<div class="topbar">
+            <div class="logo">
+                <h2>Open Library</h2>
+            </div>
+            <div class="search">
+                <input type="text" id="search" aria-placeholder="search here">
+                <label for="search"><i class="fas fa-search"></i></label>
+
+            </div>
+            <div class="imtop" >
+                <div style="margin-right: 5px;"> <i class="fas fa-bell"></i></div>
+                <div class="user">
+                    <img src="images/admin.png" />
+
+                </div>
+            </div>
+        </div>
